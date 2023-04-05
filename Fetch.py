f2510cc8500502825377390a173fd6dd44ea5b6a
IttsGod <91871891+IttsGod@users.noreply.github.com>
2023-04-06 00:49:15 +0200

    Update v1.3
    Added Last Updated Colum
    Only Update if  the last Update was more than 24 Hours away.
    Made Delay smaller
    Updated Excel File every Request instead of only at the End

--- Fetch.py
+++ Fetch.py
@@ -7,8 +7,10 @@ import time
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill, numbers
+from datetime import datetime, timedelta
 
 #Global Variable Declaration
+
 global language
 global file_name
 global currency
@@ -66,39 +68,47 @@ session = requests.Session()
 
 # Iterate through the rows, starting from the second row (skipping the header)
 total_profit = 0
-for row in range(2, worksheet.max_row  + 1):
+for row in range(2, worksheet.max_row + 1):
     skin_name = worksheet.cell(row=row, column=1).value
     if skin_name is not None:
-        parse_skin_name = urllib.parse.quote(skin_name)
-        market_hash_name = get_market_hash_name(parse_skin_name, session)
-        time.sleep(3)  # Add delay after get_market_hash_name
-        if market_hash_name is not None:
-            market_hash_name = urllib.parse.quote(market_hash_name)
-            price = get_skin_price(market_hash_name, session)
-            time.sleep(3)  # Add delay after get_skin_price
-            if price is not None:
-                cell = worksheet.cell(row=row, column=4, value=price)
-                cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
-                purchase_price = worksheet.cell(row=row, column=3).value
-                profit = price - purchase_price
-                profit_cell = worksheet.cell(row=row, column=5, value=profit)
-                profit_cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
-                amount = worksheet.cell(row=row, column=2).value
-                total_profit_per_item = profit * amount
-                total_profit += total_profit_per_item
-                total_profit_cell = worksheet.cell(row=row, column=6, value=total_profit_per_item)
-                total_profit_cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
-                print("Successfully got Price for " + skin_name)
+        last_updated_cell = worksheet.cell(row=row, column=7)
+        last_updated = last_updated_cell.value
+        update_price = True
 
+        if last_updated is not None:
+            time_since_last_update = datetime.now() - last_updated
+            if time_since_last_update.total_seconds() < timedelta(hours=24).total_seconds():
+                update_price = False
 
-            else:
-                worksheet.cell(row=row, column=4, value="Not Found")
-                print("Couldnt find Price for Item: " + skin_name + ". Please check your Spelling, and if this happens often, try again in 2 Minutes")
+        if update_price:
+            parse_skin_name = urllib.parse.quote(skin_name)
+            market_hash_name = get_market_hash_name(parse_skin_name, session)
+            time.sleep(2)  # Add delay after get_market_hash_name
+
+            if market_hash_name is not None:
+                market_hash_name = urllib.parse.quote(market_hash_name)
+                price = get_skin_price(market_hash_name, session)
+                time.sleep(2)  # Add delay after get_skin_price
+                if price is not None:
+                    cell = worksheet.cell(row=row, column=4, value=price)
+                    cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
+                    purchase_price = worksheet.cell(row=row, column=3).value
+                    profit = price - purchase_price
+                    profit_cell = worksheet.cell(row=row, column=5, value=profit)
+                    profit_cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
+                    amount = worksheet.cell(row=row, column=2).value
+                    total_profit_per_item = profit * amount
+                    total_profit += total_profit_per_item
+                    total_profit_cell = worksheet.cell(row=row, column=6, value=total_profit_per_item)
+                    total_profit_cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
+                    print("Successfully got Price for " + skin_name)
+                    last_updated_cell.value = datetime.now()
+                    # Save the updated Excel file
+                    workbook.save(excel_file_path)
+                else:
+                    print("Couldn't find Price for Item: " + skin_name + ". Please check your Spelling, and if this happens often, try again in 2 Minutes")
         else:
-            print("Couldnt find Price for Item: " + skin_name + ". Please check your Spelling, and if this happens often, try again in 2 Minutes")
-
-all_total_profit_cell = worksheet.cell(row=2, column=7, value=total_profit)
-all_total_profit_cell.number_format = f'#,##0.00\ "{currency_token}";[Red]\-#,##0.00\ "{currency_token}"'
+            print("Skipping update for Item: " + skin_name + " as it was updated within the last 24 hours")
 
 # Save the updated Excel file
 workbook.save(excel_file_path)
